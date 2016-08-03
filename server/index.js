9fbf93f6c330f60f7ea429a2b262c7e9bb8b35bd
Arjan Einbu <arjan@einbu.no>
2016-08-04 00:15:49 +0200

    Pseudecode

---
+++ server/index.js
@@ -0,0 +1,33 @@
+var express = require("express");
+var exec = require("child_process").exec;
+var bodyparser = require("body-parser");
+
+var app = express();
+
+app.post("github-hook",
+	bodyparser.json(),
+	function(req, resp) {
+
+		var postdata = req.body;
+
+		// look up project in config.json
+		// clone buildprocess-repo to sandbox (if not exists)
+		// EXEC builprocess ("node build.js") Setup stdin, stdout and stderr
+		//   create workspace (if not exists)
+		//   load deps (nuget/npm etc.)
+		//   run .js in workspace
+	}
+);
+
+app.get("dashboard", function(req, resp){
+	// list of projects, statuses, disable/enable, buttons to start/pause/stop a build, link to logs/output
+});
+
+app.get("log/:projectId/:version?", function(req, resp){
+	// show latest log
+	// allow navigating to other logs
+});
+
+app.listen(3000, function () {
+  console.log('Scriptabuild http server listening on port 3000!');
+});
