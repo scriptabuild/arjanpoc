0ccb25cc9d3f7e24d4e79658d9f6d6839c4ffb12
Arjan Einbu <arjan@einbu.no>
2016-08-13 14:31:32 +0200

    Refactoring

--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@ const exec = require("child_process").exec;
 const bodyparser = require("body-parser");
 const _ = require("lodash");
 
-const projects = require("./projects");
 
 
 
@@ -11,61 +10,66 @@ var app = express();
 
 app.use("/assets", express.static(__dirname + "/wwwroot", { extensions: ["css", "jpg", "png"] }));
 
-app.get("/", function(req, resp){
+app.get("/", function (req, resp) {
 	resp.redirect("/app");
 })
 
 app.get("/app*", function (req, resp) {
-	resp.sendfile(__dirname + "/wwwroot/index.html");
+	resp.sendFile(__dirname + "/wwwroot/index.html");
 });
 
 app.get("/project-list", function (req, resp) {
-	let projects = [
+	const projects = require("./projects");
+
+	resp.json(_(projects).map(p => {return { name: p.name, status: "ok" };}) );
+});
+
+app.get("/project-detail/:projectName", function (req, resp) {
+	let project =
 		{
 			name: "oktaset cfp Self Service",
-			status: "ok"
-		}, {
-			name: "oktaset cfp Admin",
-			status: "ok"
-		}, {
-			name: "metamory",
-			status: "ok"
-		}, {
-			name: "react-playground",
-			status: "failed"
-		}, {
-			name: "browserify-playground",
-			status: "ok"
-		}, {
-			name: "dotnetcore-playground",
-			status: "ok"
-		}
-	];
+			status: "ok",
+			builds: [
+				{
+					commitId: "52b8775",
+					date: "2016-08-01T12:00:00+02:00",
+					status: "ok"
+				},
+				{
+					commitId: "5ace85c",
+					date: "2016-08-01T11:58:22+02:00",
+					status: "failed"
+				}
+			]
+		};
 
-	resp.json(projects);
+	resp.json(project);
 });
 
+// app.post("/project-build/:projectName", function(req, resp){
+// 	// Trigger the build. Similar to a webhook
+// });
 
-// 1. boot the server -> create folders and set up hooks endpoint
+// // 1. boot the server -> create folders and set up hooks endpoint
 
-app.post("github-hook",
-	bodyparser.json(),
-	function (req, resp) {
+// app.post("github-hook",
+// 	bodyparser.json(),
+// 	function (req, resp) {
 
-		var postdata = req.body;
+// 		var postdata = req.body;
 
-		// 2. create workspace, checkout build script, run build script...
-	}
-);
+// 		// 2. create workspace, checkout build script, run build script...
+// 	}
+// );
 
-app.get("dashboard", function (req, resp) {
-	// list of projects, statuses, disable/enable, buttons to start/pause/stop a build, link to logs/output
-});
+// app.get("dashboard", function (req, resp) {
+// 	// list of projects, statuses, disable/enable, buttons to start/pause/stop a build, link to logs/output
+// });
 
-app.get("log/:projectId/:version?", function (req, resp) {
-	// show latest log
-	// allow navigating to other logs
-});
+// app.get("log/:projectId/:version?", function (req, resp) {
+// 	// show latest log
+// 	// allow navigating to other logs
+// });
 
 app.listen(3000, function () {
 	console.log('Scriptabuild http server listening on port 3000!');
